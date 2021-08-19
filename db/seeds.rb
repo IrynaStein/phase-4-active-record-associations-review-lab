aaf1cf493c95bf5e6ce3d5692b46affd8e78af3e
Iryna Stein <isophiestein@gmail.com>
2021-08-19 11:47:15 -0600

    commit tree aa00dba88d80aa6e7bb2a1f2d4406cb707755247

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Taxi.destroy_all
+Passenger.destroy_all
+Ride.destroy_all
+
+puts "Seeding taxis  ...."
+10.times do
+    Taxi.create(vin_number: rand(1234..5436))
+end
+
+puts "Seeding passengers..."
+names = %w[matt george pete alan veronica sarah jane susan isabella john travis jack]
+15.times do 
+    Passenger.create(name: names.sample)
+end
+
+puts "Seeding rides..."
+
+20.times do 
+    Ride.create(taxi_id: rand(1..10), passenger_id: rand(1..15))
+end
+
+puts "Done..."
